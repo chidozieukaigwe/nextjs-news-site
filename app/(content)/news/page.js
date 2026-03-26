493b980dc0c4e3519edf017d72918ef338f922c3
chido <chidodesigns@gmail.com>
2026-03-26 15:39:30 +0000

    fetching data directly from the source - as we own data source and db

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,16 +1,8 @@
 import NewsList from "@/components/news/news-list";
+import { getAllNews } from "@/lib/news";
 
 export default async function NewsPage() {
-  /**
-   * We are making a server side request to fetch the news data from our backend API. We can acheive this as this is server component and we can make direct calls to server side resources. We now do not have to make/utilize client side code like useState and useEffect to fetch the data. We can directly fetch the data and render it on the server side. This is one of the main advantages of using server components in Next.js 13. We can fetch data directly on the server and render it without having to worry about client side rendering and hydration issues.
-   */
-  const response = await fetch("http://localhost:8080/news");
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch news data");
-  }
-
-  const news = await response.json();
+  const news = getAllNews();
 
   return (
     <div id="news">
